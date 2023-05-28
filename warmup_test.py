70f46c149f06d656422972ca835c228a71d74849
nh1019 <nathan.hamill19@imperial.ac.uk>
2023-05-28 21:23:23 +0100

    using different early stopping parameters based on dataset

--- warmup_test.py
+++ warmup_test.py
@@ -59,15 +59,15 @@ def train_AE(mode: str, dataset: str, batch_size: int, epochs: int, encoded_dim:
     if dataset=='MNIST':
         channels = 1
         trainloaders = prepare_MNIST(mode, batch_size, train_transform)
+        es = EarlyStopper(min_delta=.05)
     elif dataset=='CIFAR':
         channels = 3
         trainloaders = prepare_CIFAR(mode, batch_size, train_transform)
+        es = EarlyStopper(min_delta=.005)
 
     worker_losses = {0: [], 1: [], 2: [], 3: [], 4: []}
     encoders = [Encoder(channels, encoded_dim).to(device) for _ in range(n_workers)]
     decoders = [Decoder(channels, encoded_dim).to(device) for _ in range(n_workers)]
-    
-    es = EarlyStopper(min_delta=0.1)
 
     params_to_optimize = []
     for i in range(n_workers):
