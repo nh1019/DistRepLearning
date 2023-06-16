45894799ce0208920a51df07967402bf75993556
nh1019 <60328690+nh1019@users.noreply.github.com>
2023-06-16 19:53:27 +0100

    adding iid option to MNIST

--- utils/prepare_dataloaders.py
+++ utils/prepare_dataloaders.py
@@ -3,12 +3,21 @@ from torchvision import transforms, datasets
 from torch.utils.data import DataLoader, Subset
 
 
-def prepare_MNIST(mode: str, batch_size: int, train_transform=None, train=True, data_fraction=1.):
+def prepare_MNIST(mode: str, batch_size: int, train_transform=None, train=True, iid=False, data_fraction=1.):
     test_transform = transforms.Compose([transforms.ToTensor()])
     worker_classes = [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
 
     if train:
         train_dataset = datasets.MNIST(root='./data', train=True, transform=train_transform, download=True)
+
+        if iid:
+            if data_fraction<1.:
+                num_samples = int(data_fraction*len(train_dataset))
+                train_dataset = torch.utils.data.random_split(train_dataset, [num_samples, len(train_dataset)-num_samples])[0]
+                
+            l = int(len(train_dataset)/5)
+            subsets = [Subset(train_dataset, range(i*l,(i+1)*l)) for i in range(0,5)]
+            return [DataLoader(subset, batch_size, shuffle=True, drop_last=True) for subset in subsets]
 
         if mode=='centralized':
             if data_fraction<1.:
